f3867b2c58866c525985c512b3166522e7c1c6df
Goran Hellberg <cosmos3@telia.com>
2014-09-28 18:02:07 +0200

    Update

--- webroot/config.php
+++ webroot/config.php
@@ -4,7 +4,7 @@
 	include(ANAX_APP_PATH."config/autoloader.php");
 	include(ANAX_INSTALL_PATH."src/functions.php");
 	$server="http://".$_SERVER['SERVER_NAME'];
-	define('EGO_URL_ROOT', $server); // local
+	//define('EGO_URL_ROOT', $server); // local
 	//define('EGO_URL_ROOT', $server."/bth_phpmvc"); // cosmos3.se
-	//define('EGO_URL_ROOT', $server."/~gohe14/phpmvc/kmom01"); // BTH
+	define('EGO_URL_ROOT', $server."/~gohe14/phpmvc/kmom01"); // BTH
 ?>
